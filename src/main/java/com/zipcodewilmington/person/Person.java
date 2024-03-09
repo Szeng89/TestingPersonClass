d1ee26f3cfdc10946ba8faaf9de32aeda3f80fc5
Szeng89 <shijinz898@gmail.com>
2024-03-09 12:51:29 -0500

    added instance varibles? in the fields

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,73 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private String lastName;
+    private boolean hasBrain;
+
+
+
 
     public Person() {
+        name = "";
+        age = Integer .MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, int height){
+        this.name =name;
+        this.age =age;
+        this.height =height;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isHasBrain() {
+        return hasBrain;
+    }
+
+    public void setHasBrain(boolean hasBrain) {
+        this.hasBrain = hasBrain;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -37,6 +37,7 @@ public class TestPerson {
         Assert.assertEquals(expected, actual);
     }
 
+
     @Test
     public void testConstructorWithAge() {
         // Given
@@ -52,20 +53,23 @@ public class TestPerson {
 
 
     @Test
-    public void testConstructorWithNameAndAge() {
+    public void testConstructorWithEverything() {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
+        Integer expectedHeight = 5;
 
         // When
-        Person person = new Person(expectedName, expectedAge);
+        Person person = new Person(expectedName, expectedAge, expectedHeight);
 
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
+        Integer actualHeight = person.getHeight();
 
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedHeight, actualHeight);
     }
 
     @Test
@@ -93,6 +97,50 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        Integer actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLastName() {
+        // Given
+        Person person = new Person();
+        String expected = "zeng";
+
+        // When
+        person.setLastName(expected);
+
+
+        // Then
+        String actual = person.getLastName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHasBrain() {
+        // Given
+        Person person = new Person();
+        Boolean expected = true;
+
+        // When
+        person.setHasBrain(expected);
+
+
+        // Then
+        boolean actual = person.isHasBrain();
         Assert.assertEquals(expected, actual);
     }
 }
